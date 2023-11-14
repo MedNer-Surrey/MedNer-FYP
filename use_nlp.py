f56a43917ede9b1460827bf5bc488a8164b2639f
PavloNa <pavlo.escola@gmail.com>
2023-11-14 18:44:09 +0000

    Broader test

--- use_nlp.py
+++ use_nlp.py
@@ -2,16 +2,11 @@ import spacy
 
 def main():
     nlp = spacy.load("output/model-best")
-    text = "Antiretroviral therapy (ART) is recommended for all HIV-infected\
-individuals to reduce the risk of disease progression.\nART also is recommended \
-for HIV-infected individuals for the prevention of transmission of HIV.\nPatients \
-starting ART should be willing and able to commit to treatment and understand the\
-benefits and risks of therapy and the importance of adherence. Patients may choose\
-to postpone therapy, and providers, on a case-by-case basis, may elect to defer\
-therapy on the basis of clinical and/or psychosocial factors. HIV is a disease."
+    file=open("test.txt","r")
+    text = file.read()
     doc = nlp(text)
-    colors = {"PATHOGEN": "#F67DE3", "MEDICINE": "#7DF6D9", "MEDICALCONDITION":"#FFFFFF"}
-    options = {"colors": colors} 
+    colors = {"PATHOGEN": "#870A30", "MEDICINE": "#43B0F1", "MEDICALCONDITION":"#B1D8B7"}
+    options = {"ents" : ["PATHOGEN", "MEDICINE", "MEDICALCONDITION"] ,"colors": colors} 
     data = spacy.displacy.render(doc, style="ent", options= options, jupyter=False)
     with open("data.html", "w") as file:
         file.write(data)
